ab575ef06cce98ef7b9f913439a9c603f957247d
Ahmad <amadsabah6@gmail.com>
2026-09-13 14:16:22 +0300

    first commit

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -36,6 +36,7 @@ export default function Profile() {
               type="button"
               className="gap-x-1 before:ease relative overflow-hidden border border-[#000000] bg-[#050708] text-white shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:before:-translate-x-40 font-medium rounded-lg text-sm px-2 py-2 text-center inline-flex items-center mr-2 mb-2"
             >
+              {/* Ahmad */}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
